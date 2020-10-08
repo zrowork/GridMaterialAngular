492047dce2b0d38836b05b6494fade437563f19e
olo <akroworz2@gmail.com>
2020-10-08 21:26:15 +0200

    Add material angular imodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MondrianPaintingComponent } from './grid-examples/mondrian-painting/mondrian-painting.component';
 import { MondrainPaintingByItselfComponent } from './grid-examples/mondrain-painting-by-itself/mondrain-painting-by-itself.component';
+import { GridAreasWithMatAngularComponent } from './grid-examples/grid-areas-with-mat-angular/grid-areas-with-mat-angular.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+const material = [MatButtonModule, MatFormFieldModule, MatInputModule];
 
 @NgModule({
-  declarations: [AppComponent, MondrianPaintingComponent, MondrainPaintingByItselfComponent],
-  imports: [
-    MatButtonModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
+  declarations: [
+    AppComponent,
+    MondrianPaintingComponent,
+    MondrainPaintingByItselfComponent,
+    GridAreasWithMatAngularComponent,
   ],
+  imports: [material, BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  exports: [material],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/grid-examples/grid-areas-with-mat-angular/grid-areas-with-mat-angular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-areas-with-mat-angular',
+  templateUrl: './grid-areas-with-mat-angular.component.html',
+  styleUrls: ['./grid-areas-with-mat-angular.component.scss']
+})
+export class GridAreasWithMatAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
